aed3f02a9a42a976fe36511a61c9e3fa273fe24c
Herry Nguyen <herry.nguyen@infonam.com>
2015-12-29 09:42:05 +0700

    [Branch 1] edit file b1

--- b1.java
+++ b1.java
@@ -1,22 +1,5 @@
 package sight;
 
-import java.io.*;
-import java.lang.reflect.Array;
-import java.net.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Element;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import static sun.org.mozilla.javascript.internal.ScriptRuntime.name;
 
 public class ServerCode {
 
